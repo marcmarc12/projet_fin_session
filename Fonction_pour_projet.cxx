840da323efc88a197be9a5a1fac3a27af7ab5789
Marc-Antoine <m-a.boisvert@hotmail.com>
2019-12-04 07:42:46 -0500

    2019-12-04_7h42

---
+++ Fonction_pour_projet.cxx
@@ -0,0 +1,10 @@
+/*
+ * Fonction_pour_projet.cxx
+ *
+ *  Created on: 4 déc. 2019
+ *      Author: Marc-Antoine
+ */
+
+#include "Fonction_pour_projet.h"
+#include "FC_de_base.h"
+
---
+++ Fonction_pour_projet.h
@@ -0,0 +1,20 @@
+/*
+ * Fonction_pour_projet.h
+ *
+ *  Created on: 4 déc. 2019
+ *      Author: Marc-Antoine
+ */
+
+#include <iostream> //pour utiliser des éléments du c++
+#include <string>   //pour utiliser des "string"
+#include <math.h>   //permet des calculs plus poussés.
+#include <stdlib.h> //pour utiliser : NULL, EXIT_FAILURE, etc.
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#ifndef FONCTION_POUR_PROJET_H_
+#define FONCTION_POUR_PROJET_H_
+
+#endif /* FONCTION_POUR_PROJET_H_ */
